044d03d24d80f5ba8c833fcb49c70a0882aa6173
Jeonghwan <jaohoo@daum.net>
2023-11-27 13:26:25 +0900

    앱 버전 출력

--- app/src/main/java/com/cookandroid/registeration/ui/PopActivity.java
+++ app/src/main/java/com/cookandroid/registeration/ui/PopActivity.java
@@ -1,8 +1,10 @@
 package com.cookandroid.registeration.ui;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.TextView;
 
 import com.cookandroid.registeration.R;
 
@@ -11,6 +13,14 @@ public class PopActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_pop);
+
+        TextView textViewAppVersion = findViewById(R.id.tv_app_version);
+        try {
+            String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            textViewAppVersion.setText(" 어플리케이션 정보 : " + versionName + " Ver");
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
         DisplayMetrics dm=new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
